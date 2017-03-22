53541864be83e9105f06962a4aae90b6a932146b
mzliew <mzliew@ualberta.ca>
2017-03-22 17:39:17 -0600

    merge master

--- app/src/main/java/com/example/moodly/Activities/ViewComments.java
+++ app/src/main/java/com/example/moodly/Activities/ViewComments.java
@@ -11,5 +11,11 @@ public class ViewComments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_comments);
+        showComments();
     }
+
+    protected void showComments(){
+        return;
+    }
+
 }
