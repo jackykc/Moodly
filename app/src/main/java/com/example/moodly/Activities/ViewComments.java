b7cfc79305a638125e2714c48f6e49c9dd83a39a
mzliew <mzliew@ualberta.ca>
2017-03-22 19:47:28 -0600

    assuming commenting works

--- app/src/main/java/com/example/moodly/Activities/ViewComments.java
+++ app/src/main/java/com/example/moodly/Activities/ViewComments.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.example.moodly.Controllers.CommentController;
 import com.example.moodly.Models.Comment;
@@ -14,21 +16,27 @@ import java.util.ArrayList;
 public class ViewComments extends AppCompatActivity {
     ArrayList<Comment> commentList = new ArrayList<>();
     ListView displayCommentList;
+    TextView displayNoComment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_comments);
+        android.support.v7.app.ActionBar action = getSupportActionBar();
+        action.setTitle("Comments");
         String id = getIntent().getStringExtra("moodID");
         commentList = CommentController.getInstance().getCommentList(id);
         showComments(commentList);
     }
 
     protected void showComments(ArrayList<Comment> commentList){
+        ViewSwitcher viewSwitcher = (ViewSwitcher)findViewById(R.id.messageSwitcher);
+        displayCommentList = (ListView) findViewById(R.id.commentsView);
+        displayNoComment = (TextView) findViewById(R.id.noComments);
         if (commentList.size() == 0){
-            // if there isn't any comments, print message to tell user?
+            viewSwitcher.showNext();
         }
         else {
-            displayCommentList = (ListView) findViewById(R.id.commentsView);
+            // arrayadapter for now until CommentAdapter is created
             ArrayAdapter<Comment> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, commentList);
             displayCommentList.setAdapter(adapter);
         }
